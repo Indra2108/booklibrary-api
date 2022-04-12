071a361806920d6143f01d8013fd684b5dfe4faa
Indra2108 <fizipratama21@gmail.com>
2022-04-12 10:43:21 +0700

    Router: adding getBooks routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,16 @@
 const addBooks = require('../handler/addBooks')
+const getBooks = require('../handler/getBooks')
 
 const routes = [
     {
         method: 'POST',
         path: '/books',
         handler: addBooks
+    },
+    {
+        method: 'GET',
+        path: '/books',
+        handler: getBooks
     }
 ]
 
