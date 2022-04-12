5cee0c6a51160569d316c5d1b04a65a0ad0f32b3
Indra2108 <fizipratama21@gmail.com>
2022-04-12 10:30:10 +0700

    Handler: Update addBooks Change response code to 500 expanding try and catch

--- src/handler/addBooks.js
+++ src/handler/addBooks.js
@@ -2,37 +2,38 @@ const { nanoid } = require("nanoid")
 const booksLibrary = require('../assets/books')
 
 const addBooks = (request, h) => {
-    const {
-        name,
-        year,
-        author,
-        summary,
-        publisher,
-        pageCount,
-        readPage,
-        reading
-    } = request.payload
-    const id = nanoid(8)
-    const insertedAt = new Date().toISOString()
-    const updatedAt = insertedAt
-    const finished = pageCount === readPage
-
-    const newBook = {
-        id,
-        name,
-        year,
-        author,
-        summary,
-        publisher,
-        pageCount,
-        readPage,
-        finished,
-        reading,
-        insertedAt,
-        updatedAt
-    }
-
     try {
+        const {
+            name,
+            year,
+            author,
+            summary,
+            publisher,
+            pageCount,
+            readPage,
+            reading
+        } = request.payload
+        const id = nanoid(8)
+        const insertedAt = new Date().toISOString()
+        const updatedAt = insertedAt
+        const finished = pageCount === readPage
+
+        const newBook = {
+            id,
+            name,
+            year,
+            author,
+            summary,
+            publisher,
+            pageCount,
+            readPage,
+            finished,
+            reading,
+            insertedAt,
+            updatedAt
+        }
+
+
         if (name === undefined || name === '' || name === null) {
             const response = h.response({
                 status: 'fail',
@@ -72,7 +73,7 @@ const addBooks = (request, h) => {
             message: "Buku gagal ditambahkan",
             messageError: error
         })
-        response.code(400)
+        response.code(500)
         return response
     }
 
