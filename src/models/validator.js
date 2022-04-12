c0d00aba7e23e4afe70738818b9afc1b6c79548a
Indra2108 <fizipratama21@gmail.com>
2022-04-12 23:20:54 +0700

    Models: add Data Schema Validator

---
+++ src/models/validator.js
@@ -0,0 +1,35 @@
+const Joi = require("joi")
+
+const inputDataSchema = Joi.object({
+    name: Joi.string(),
+    year: Joi.number(),
+    author: Joi.string(),
+    summary: Joi.string(),
+    publisher: Joi.string(),
+    pageCount: Joi.number(),
+    readPage: Joi.number(),
+    reading: Joi.boolean()
+})
+
+const outputDataSchema = Joi.object({
+    id: Joi.string(),
+    name: Joi.string(),
+    year: Joi.number(),
+    author: Joi.string(),
+    summary: Joi.string(),
+    publisher: Joi.string(),
+    pageCount: Joi.number(),
+    readPage: Joi.number(),
+    finished: Joi.boolean(),
+    reading: Joi.boolean(),
+    insertedAt: Joi.date().iso(),
+    updatedAt: Joi.date().iso()
+})
+
+const simpleOutputDataSchema = Joi.object({
+    id: Joi.string(),
+    name: Joi.string(),
+    publisher: Joi.string()
+})
+
+module.exports = { inputDataSchema, outputDataSchema, simpleOutputDataSchema }
