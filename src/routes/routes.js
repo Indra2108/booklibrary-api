a81df096814db53ff959937d89339b519b68c026
Indra2108 <fizipratama21@gmail.com>
2022-04-12 23:31:48 +0700

    Router: refactoring code Import Joi Import deleteBooks handler Import DataSchema Add Validation per route, except DELETE method Add DELETE method

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,28 +1,63 @@
+// Library
+const Joi = require('joi')
+
+// Handler
 const addBooks = require('../handler/addBooks')
 const getBooks = require('../handler/getBooks')
 const getBooksById = require('../handler/getBooksById')
 const editBooks = require('../handler/editBooks')
+const deleteBooks = require('../handler/deleteBooks')
+
+// Data Schema
+const { inputDataSchema, outputDataSchema, simpleOutputDataSchema } = require('../models/validator')
 
 const routes = [
     {
         method: 'POST',
         path: '/books',
-        handler: addBooks
+        handler: addBooks,
+        options: {
+            validate: {
+                payload: inputDataSchema
+            },
+        }
     },
     {
         method: 'GET',
         path: '/books',
-        handler: getBooks
+        handler: getBooks,
+        options: {
+            response: {
+                schema: Joi.array().items(simpleOutputDataSchema),
+                failAction: 'log'
+            }
+        }
     },
     {
         method: 'GET',
         path: '/books/{bookId}',
-        handler: getBooksById
+        handler: getBooksById,
+        options: {
+            response: {
+                schema: Joi.array().items(outputDataSchema),
+                failAction: 'log'
+            }
+        }
     },
     {
         method: 'PUT',
         path: '/books/{bookId}',
-        handler: editBooks
+        handler: editBooks,
+        options: {
+            validate: {
+                payload: inputDataSchema
+            },
+        }
+    },
+    {
+        method: 'DELETE',
+        path: '/books/{bookId}',
+        handler: deleteBooks
     }
 ]
 
