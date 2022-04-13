9fee558515edb8e5c2a71f8b461119f8aebe6312
Indra2108 <fizipratama21@gmail.com>
2022-04-13 22:26:55 +0700

    Handler: getBooks: Add request query handler

--- src/handler/getBooks.js
+++ src/handler/getBooks.js
@@ -2,6 +2,104 @@ const booksLibrary = require('../assets/books')
 
 const getBooks = (request, h) => {
     try {
+        const { name, reading, finished } = request.query
+
+        // if name parameter exist
+        if (name !== undefined) {
+            const response = h.response({
+                status: 'success',
+                message: 'success search name',
+                data: {
+                    booksLibrary: booksLibrary
+                        .filter(data => data.name.toLowerCase().includes(name.toLowerCase()))
+                        .map(b => ({
+                            id: b.id,
+                            name: b.name,
+                            publisher: b.publisher
+                        }))
+                }
+            })
+            response.code(200)
+            return response
+
+        }
+
+        // reading is false / 0
+        if (reading === '0') {
+            const response = h.response({
+                status: 'success',
+                message: 'success search not reading book',
+                data: {
+                    booksLibrary: booksLibrary
+                        .filter(({ reading }) => reading === false)
+                        .map(b => ({
+                            id: b.id,
+                            name: b.name,
+                            publisher: b.publisher
+                        }))
+                }
+            })
+            response.code(200)
+            return response
+        }
+
+        // reading is true / 1
+        if (reading === '1') {
+            const response = h.response({
+                status: 'success',
+                message: 'success search reading book',
+                data: {
+                    booksLibrary: booksLibrary
+                        .filter(({ reading }) => reading === true)
+                        .map(b => ({
+                            id: b.id,
+                            name: b.name,
+                            publisher: b.publisher
+                        }))
+                }
+            })
+            response.code(200)
+            return response
+        }
+
+        // finished is false / 0
+        if (finished === '0') {
+            const response = h.response({
+                status: 'success',
+                message: 'success search unfinished book',
+                data: {
+                    booksLibrary: booksLibrary
+                        .filter(({ finished }) => finished === false)
+                        .map(b => ({
+                            id: b.id,
+                            name: b.name,
+                            publisher: b.publisher
+                        }))
+                }
+            })
+            response.code(200)
+            return response
+        }
+
+        // finished is true / 1
+        if (finished === '1') {
+            const response = h.response({
+                status: 'success',
+                message: 'success search finished book',
+                data: {
+                    booksLibrary: booksLibrary
+                        .filter(({ finished }) => finished === true)
+                        .map(b => ({
+                            id: b.id,
+                            name: b.name,
+                            publisher: b.publisher
+                        }))
+                }
+            })
+            response.code(200)
+            return response
+        }
+
         const response = h.response({
             status: 'success',
             data: {
@@ -23,7 +121,7 @@ const getBooks = (request, h) => {
         response.code(500)
         return response
     }
-    
+
 }
 
 module.exports = getBooks
