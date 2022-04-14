62c5f0d7d43a520abe55c9f09769809201bdd0aa
Indra2108 <fizipratama21@gmail.com>
2022-04-14 08:37:49 +0700

    Server: remove testing-only stuff

--- src/server.js
+++ src/server.js
@@ -4,12 +4,8 @@ const Hapi = require('@hapi/hapi')
 const routes = require('./routes/routes')
 
 const init = async () => {
-    // => Heroku Deploy
-    // const port = process.env.PORT || 3001
-    // const host = process.env.HOST || '0.0.0.0'
-    // => For testing only
-    const port = 5000
-    const host = 'localhost'
+    const port = process.env.PORT || 3001
+    const host = process.env.HOST || '0.0.0.0'
 
     const server = Hapi.server({
         port: port,
