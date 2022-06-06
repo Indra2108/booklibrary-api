1dbd79ac136aab92439ccfb07a5dc5e74b9eee30
Indra2108 <fizipratama21@gmail.com>
2022-06-06 20:43:11 +0700

    Handler: getBooks: Simplify the code by Dicoding Code Reviewer

--- src/handler/getBooks.js
+++ src/handler/getBooks.js
@@ -4,113 +4,30 @@ const books = require('../assets/books')
 
 const getBooks = (request, h) => {
     try {
-        const { name, reading, finished } = request.query
 
-        // if name parameter exist
-        if (name !== undefined) {
-            const response = h.response({
-                status: 'success',
-                message: 'success search name',
-                data: {
-                    books: books
-                        .filter(data => data.name.toLowerCase().includes(name.toLowerCase()))
-                        .map(b => ({
-                            id: b.id,
-                            name: b.name,
-                            publisher: b.publisher
-                        }))
-                }
-            })
-            response.code(200)
-            return response
+        const { name, reading, finished } = request.query;
 
+        let filteredBooks = books;
+
+        if (name) {
+            filteredBooks = books.filter((book) => new RegExp(name, 'i').test(book.name));
         }
 
-        // reading is false / 0
-        if (reading === '0') {
-            const response = h.response({
-                status: 'success',
-                message: 'success search not reading book',
-                data: {
-                    books: books
-                        .filter(({ reading }) => reading === false)
-                        .map(b => ({
-                            id: b.id,
-                            name: b.name,
-                            publisher: b.publisher
-                        }))
-                }
-            })
-            response.code(200)
-            return response
+        if (reading !== undefined) {
+            filteredBooks = books.filter((book) => !!Number(reading) === book.reading);
         }
 
-        // reading is true / 1
-        if (reading === '1') {
-            const response = h.response({
-                status: 'success',
-                message: 'success search reading book',
-                data: {
-                    books: books
-                        .filter(({ reading }) => reading === true)
-                        .map(b => ({
-                            id: b.id,
-                            name: b.name,
-                            publisher: b.publisher
-                        }))
-                }
-            })
-            response.code(200)
-            return response
-        }
-
-        // finished is false / 0
-        if (finished === '0') {
-            const response = h.response({
-                status: 'success',
-                message: 'success search unfinished book',
-                data: {
-                    books: books
-                        .filter(({ finished }) => finished === false)
-                        .map(b => ({
-                            id: b.id,
-                            name: b.name,
-                            publisher: b.publisher
-                        }))
-                }
-            })
-            response.code(200)
-            return response
-        }
-
-        // finished is true / 1
-        if (finished === '1') {
-            const response = h.response({
-                status: 'success',
-                message: 'success search finished book',
-                data: {
-                    books: books
-                        .filter(({ finished }) => finished === true)
-                        .map(b => ({
-                            id: b.id,
-                            name: b.name,
-                            publisher: b.publisher
-                        }))
-                }
-            })
-            response.code(200)
-            return response
+        if (finished !== undefined) {
+            filteredBooks = books.filter((book) => !!Number(finished) === book.finished);
         }
 
         const response = h.response({
             status: 'success',
             data: {
-                books: books.map(b => ({
-                    id: b.id,
-                    name: b.name,
-                    publisher: b.publisher
-                }))
-            }
+                books: filteredBooks.map(({ id, name: bookName, publisher }) => (
+                    { id, name: bookName, publisher }
+                )),
+            },
         })
         response.code(200)
         return response
